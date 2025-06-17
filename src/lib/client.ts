c18b214df2ebb861d57d36da0d24d2cada60876b
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-06-17 21:43:21 +0900

    fix ts error

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "polepole",
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || "",
 });
