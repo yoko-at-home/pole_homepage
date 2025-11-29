447308c73893073e8409bef6f0260e8838c7801a
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2025-11-29 12:09:20 +0900

    曲を追加

--- src/component/Songs/data.ts
+++ src/component/Songs/data.ts
@@ -4,20 +4,29 @@
 
 export const data = [
   {
+    imageFileName: "static/images/portfolio/2025/whenwesing_thumb.webp",
+    imageFileNameDetail: "static/images/portfolio/2025/whenwesing.webp",
+    header: "うたを　うたうとき",
+    subheader: "女声合唱曲集",
+    content: "作詞：長田 弘｜谷川 俊太郎｜新川 和江\n作曲：信長 貴富",
+    extraInfo: "ポールバニヤンのパンケーキ｜きみ歌えよ｜春",
+    extraInfo2: "",
+  },
+  {
     imageFileName: "static/images/portfolio/2025/iihi-tabidachi.jpg",
     imageFileNameDetail: "static/images/portfolio/2025/iihi-tabidachi.jpg",
     header: "いい日旅立ち",
     subheader: "",
-    content: "作詞・作曲：谷村　新司　編曲：横山潤子（予定）",
+    content: "作詞・作曲：谷村 新司　編曲：横山 潤子（予定）",
     extraInfo: "いい日旅立ち、絶体絶命、プレイバックpart2他",
-    extraInfo2: "編曲を横山潤子先生に委嘱予定",
+    extraInfo2: "編曲を横山潤子先生に委嘱",
   },
   {
     imageFileName: "static/images/portfolio/2025/nihon-kakyoku.jpg",
     imageFileNameDetail: "static/images/portfolio/2025/nihon-kakyoku.jpg",
     header: "日本抒情歌曲集",
     subheader: "女声合唱による",
-    content: "作詞：林　古渓｜大木　惇夫｜北原白秋\n作曲：成田為三｜服部　正｜山田耕筰\n編曲：林　光",
+    content: "作詞：林 古渓｜大木 惇夫｜北原 白秋\n作曲：成田為三｜服部 正｜山田 耕筰\n編曲：林 光",
     extraInfo: "浜辺の歌 / 野の羊 / この道 /  待ちぼうけ / かやの木山の",
     extraInfo2: "",
   },
